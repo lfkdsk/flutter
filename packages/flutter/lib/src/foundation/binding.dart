0691f38124b8dc4706b525c3fa4cfbaf4c0f4470
linxuebin <linxuebin.01@bytedance.com>
2019-11-14 21:54:07 +0800

    Get time from engine init to first frame

--- packages/flutter/lib/src/foundation/binding.dart
+++ packages/flutter/lib/src/foundation/binding.dart
@@ -51,6 +51,7 @@ abstract class BindingBase {
   /// observatory service extensions, if any.
   BindingBase() {
     developer.Timeline.startSync('Framework initialization');
+    frameworkInitializationdTimeMicros = developer.Timeline.now;
 
     assert(!_debugInitialized);
     initInstances();
@@ -67,6 +68,8 @@ abstract class BindingBase {
 
   static bool _debugInitialized = false;
   static bool _debugServiceExtensionsRegistered = false;
+  // BD ADD:
+  static int frameworkInitializationdTimeMicros = 0;
 
   /// The window to which this binding is bound.
   ///

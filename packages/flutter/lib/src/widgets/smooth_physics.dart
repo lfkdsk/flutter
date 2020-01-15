5e4f2bc7ce828b6f7ba5ae5c91cecdb5d05175ab
xupengzhan <xupengzhan@bytedance.com>
2020-01-15 17:39:13 +0800

    [scroll_opt] SmoothPhysics中去除customPhysics，用户自定义Physics可以通过parent设置
    N/A

--- packages/flutter/lib/src/widgets/smooth_physics.dart
+++ packages/flutter/lib/src/widgets/smooth_physics.dart
@@ -10,43 +10,21 @@ import 'smooth_simulation.dart';
 class SmoothScrollPhysics extends ScrollPhysics implements IScrollStartConsumedListener {
   static bool _isScrollStartConsumed = false;
 
-  final ScrollPhysics customPhysics;
-
-  /// 创建一个ScrollPhysics, 可以通过传入[customPhysics]使用用户自定义的滑动行为，但[createBallisticSimulation]
-  /// 方法不会使用customPhysics的Simulation，而是被[SmoothScrollSimulation]接管
-  SmoothScrollPhysics({this.customPhysics, ScrollPhysics parent}) : super(parent: parent);
+  SmoothScrollPhysics({ScrollPhysics parent}) : super(parent: parent);
 
   @override
   SmoothScrollPhysics applyTo(ScrollPhysics ancestor) {
-    return SmoothScrollPhysics(customPhysics: customPhysics, parent: buildParent(ancestor));
+    return SmoothScrollPhysics(parent: buildParent(ancestor));
   }
 
   @override
-  bool shouldAcceptUserOffset(ScrollMetrics position) => customPhysics?.shouldAcceptUserOffset(position) ?? true;
-
-  @override
-  double applyPhysicsToUserOffset(ScrollMetrics position, double offset) {
-    if (customPhysics != null)
-      return customPhysics.applyPhysicsToUserOffset(position, offset);
-    return super.applyPhysicsToUserOffset(position, offset);
-  }
-
-  @override
-  double applyBoundaryConditions(ScrollMetrics position, double value) {
-    if (customPhysics != null)
-      return customPhysics.applyBoundaryConditions(position, value);
-    return super.applyBoundaryConditions(position, value);
-  }
+  bool shouldAcceptUserOffset(ScrollMetrics position) => true;
 
   @override
   Simulation createBallisticSimulation(ScrollMetrics position, double velocity) {
     if (defaultTargetPlatform == TargetPlatform.android) {
       if (position.outOfRange) {
-        if (customPhysics != null) {
-          return customPhysics.createBallisticSimulation(position, velocity);
-        } else {
-          return super.createBallisticSimulation(position, velocity);
-        }
+        return super.createBallisticSimulation(position, velocity);
       }
       if (velocity.abs() < tolerance.velocity) return null;
       if (velocity > 0.0 && position.pixels >= position.maxScrollExtent) return null;
@@ -59,11 +37,7 @@ class SmoothScrollPhysics extends ScrollPhysics implements IScrollStartConsumedL
         tolerance: tolerance,
       );
     } else {
-      if (customPhysics != null) {
-        return customPhysics.createBallisticSimulation(position, velocity);
-      } else {
-        return super.createBallisticSimulation(position, velocity);
-      }
+      return super.createBallisticSimulation(position, velocity);
     }
   }
 

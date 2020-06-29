4f259bc300bcb2d842a6f52bf379bfb45f70b896
zhaoxuyang <zhaoxuyang.6@bytedance.com>
2020-06-29 18:55:42 +0800

    监控的对外api

---
+++ packages/flutter/lib/performance.dart
@@ -0,0 +1,29 @@
+/// The Flutter Monitor framework.
+///
+/// To use, import `package:flutter/monitor.dart`.
+///
+/// zhaoxuyang.6@bytedance.com
+///
+
+import 'dart:ui' as engine;
+
+class Performance {
+
+  static void startStackTraceSamples(){
+
+  }
+
+  static String getStackTraceSamples() {
+
+  }
+
+  static void stopStackTraceSamples(){
+
+  }
+
+  static String getHeapSnapshot(){
+
+  }
+
+
+}

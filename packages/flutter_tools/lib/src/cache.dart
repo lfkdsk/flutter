6dee6a722763bb3e21fe338c759dd0462aede9b8
sunkun <sunkun@bytedance.com>
2020-08-15 21:06:23 +0800

    feat: delete download logic about jit-release product (BD #49)

--- packages/flutter_tools/lib/src/cache.dart
+++ packages/flutter_tools/lib/src/cache.dart
@@ -1482,7 +1482,8 @@ const List<List<String>> _androidBinaryDirs = <List<String>>[
   <String>['android-arm64-release', 'android-arm64-release/artifacts.zip'],
   <String>['android-x64-profile', 'android-x64-profile/artifacts.zip'],
   <String>['android-x64-release', 'android-x64-release/artifacts.zip'],
-  <String>['android-x86-jit-release', 'android-x86-jit-release/artifacts.zip'],
+  // BD DEL:
+  // <String>['android-x86-jit-release', 'android-x86-jit-release/artifacts.zip'],
 ];
 
 const List<List<String>> _dartSdks = <List<String>> [

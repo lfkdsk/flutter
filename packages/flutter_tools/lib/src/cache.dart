3368cf71ea1e1b1c99194e2b1491337e08469411
wanglikun <wanglikun.eric@bytedance.com>
2020-09-24 16:04:32 +0800

    feat: add bytedance flutter web sdk download url

--- packages/flutter_tools/lib/src/cache.dart
+++ packages/flutter_tools/lib/src/cache.dart
@@ -790,8 +790,22 @@ class FlutterWebSdk extends CachedArtifact {
     } else if (globals.platform.isWindows) {
       platformName += 'windows-x64';
     }
-    final Uri url = Uri.parse('${cache.storageBaseUrl}/flutter_infra/flutter/$version/$platformName.zip');
-    await _downloadZipArchive('Downloading Web SDK...', url, location);
+    /// BD MOD: START
+    /// final Uri url = Uri.parse('${cache.storageBaseUrl}/flutter_infra/flutter/$version/$platformName.zip');
+    /// await _downloadZipArchive('Downloading Web SDK...', url, location);
+    if (location.existsSync()) {
+      location.deleteSync(recursive: true);
+    }
+    if (customEngineArtifacts.contains(platformName)) {
+      final String customEngineUrl = '$customEngineDownloadUrl/$customEngineVersion/';
+      globals.printStatus('Downloading from TT ' + customEngineUrl + platformName + '.zip');
+      await _downloadZipArchive('Downloading package $platformName...',
+          Uri.parse(customEngineUrl + platformName + '.zip'), location);
+    } else {
+      final Uri url = Uri.parse('${cache.storageBaseUrl}/flutter_infra/flutter/$version/$platformName.zip');
+      await _downloadZipArchive('Downloading Web SDK...', url, location);
+    }
+    /// END
     // This is a temporary work-around for not being able to safely download into a shared directory.
     for (final FileSystemEntity entity in location.listSync(recursive: true)) {
       if (entity is File) {
@@ -1709,7 +1723,7 @@ const List<List<String>> _androidBinaryDirs = <List<String>>[
   <String>['android-arm64-release', 'android-arm64-release/artifacts.zip'],
   <String>['android-x64-profile', 'android-x64-profile/artifacts.zip'],
   <String>['android-x64-release', 'android-x64-release/artifacts.zip'],
-//  <String>['android-x86-jit-release', 'android-x86-jit-release/artifacts.zip'],
+//  <String>['android-x86-jit-release', android-x86-jit-release/artifacts.zip'],
   // BD ADD START:
   // FIXME: Temporary delete for BD #56
   // <String>['android-arm-dynamicart-release', 'android-arm-dynamicart-release/artifacts.zip'],

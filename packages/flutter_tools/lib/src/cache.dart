db478a58e985eafd1fb2bc40b748302dcd2352e5
wanglikun <wanglikun.eric@bytedance.com>
2020-10-13 11:48:52 +0800

    revert: revert close lites mode

--- packages/flutter_tools/lib/src/cache.dart
+++ packages/flutter_tools/lib/src/cache.dart
@@ -1203,7 +1203,7 @@ class IOSEngineLiteArtifacts extends EngineCachedArtifact {
       return const <String>[
         'ios-release-lite',
         'ios-release-liteg',
-//        'ios-release-lites',
+        'ios-release-lites',
 //        'ios-dynamicart-release-lites'
 ];
     }
@@ -1589,7 +1589,7 @@ const List<List<String>> _iosBinaryDirs = <List<String>>[
 const List<List<String>> _iosBinaryLiteDirs = <List<String>>[
   <String>['ios-release-lite', 'ios-release-lite/artifacts.zip'],
   <String>['ios-release-liteg', 'ios-release-liteg/artifacts.zip'],
-// <String>['ios-release-lites', 'ios-release-lites/artifacts.zip'],
+  <String>['ios-release-lites', 'ios-release-lites/artifacts.zip'],
 // <String>['ios-dynamicart-release-lites', 'ios-dynamicart-release-lites/artifacts.zip'],
 ];
 // END

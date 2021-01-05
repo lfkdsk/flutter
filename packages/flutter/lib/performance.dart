71f36b184958bd572025b30654eeee23086fea8a
linxuebin <linxuebin.01@bytedance.com>
2021-01-05 16:20:55 +0800

    feat: Add memory interface

--- packages/flutter/lib/performance.dart
+++ packages/flutter/lib/performance.dart
@@ -14,6 +14,7 @@ import 'dart:ui' show performance;
 
 import 'package:flutter/src/performance/heap_snapshot.dart';
 import 'package:flutter/src/widgets/binding.dart';
+import 'package:flutter/src/painting/binding.dart';
 
 class LowMemoryObserver extends WidgetsBindingObserver {
   String outFilePath;
@@ -122,5 +123,74 @@ class Performance {
   /// Memory usage of decoded image in dart heap external, in KB
   static int getImageMemoryKB() {
     return performance.getImageMemoryUsage();
+  }
+
+  static List GetSkGraphicCacheMemoryKB() {
+    return performance.getSkGraphicCacheMemoryUsage();
+  }
+
+  static Future<List> GetGrResourceCacheMemKB() {
+    return performance.getGrResourceCacheMemInfo();
+  }
+
+  static Future<List> GetTotalExtMemInfoKB() {
+    return performance.getTotalExtMemInfo();
+  }
+
+  static Future<Map> GetTotalMemInfoMap() async {
+    String jsonString = getHeapUsageJSON();
+    final isolates = jsonDecode(jsonString);
+
+    int used = 0;
+    int capacity = 0;
+    int externl = 0;
+    int gc_count = 0;
+    int gc_time = 0;
+    int gc_count_old = 0;
+    int gc_time_old = 0;
+    for (var isolate in isolates) {
+      used += isolate['heaps']['new']['used'];
+      used += isolate['heaps']['old']['used'];
+
+      capacity += isolate['heaps']['new']['capacity'];
+      capacity += isolate['heaps']['old']['capacity'];
+
+      externl += isolate['heaps']['new']['external'];
+      externl += isolate['heaps']['old']['external'];
+
+      gc_count = isolate['heaps']['new']['gcCount'];
+      gc_time = isolate['heaps']['new']['gcTime'];
+
+      gc_count_old = isolate['heaps']['old']['gcCount'];
+      gc_time_old = isolate['heaps']['old']['gcTime'];
+    }
+
+
+    List extMenList = await GetTotalExtMemInfoKB();
+
+    int imageCache = PaintingBinding.instance?.imageCache?.currentSizeBytes >> 10;
+    int imageLive = PaintingBinding.instance?.imageCache?.getImageLiveBytesSize() >> 10;
+    return {
+      'heap_capacity': capacity,
+      'heap_used': used,
+      'external_usage': externl,
+      'image_usage': extMenList[0],
+      'gpu_cache': extMenList[1],
+      'gpu_cache_buddget': extMenList[2],
+      'gpu_cache_purgeable': extMenList[3],
+      'io_gpu_cache': extMenList[4],
+      'io_gpu_cache_buddget': extMenList[5],
+      'io_gpu_cache_purgeable': extMenList[6],
+      'bitmap_cache': extMenList[7],
+      'font_cache': extMenList[8],
+      'image_filter': extMenList[9],
+      'sk_malloc_mem': extMenList[10],
+      'image_cache': imageCache,
+      'image_live': imageLive,
+      'gc_count': gc_count,
+      'gc_time': gc_time,
+      'gc_count_old': gc_count_old,
+      'gc_time_old': gc_time_old
+    };
   }
 }
